5176372040b562f6dfe5bc7ec5c15b10be5cc121
cenomla <linuxFTW49@gmail.com>
2017-07-17 16:10:44 -0600

    refractored sprite class

--- core/graphics/sprite.h
+++ core/graphics/sprite.h
@@ -6,19 +6,14 @@
 namespace oak::graphics {
 
 
-	class Sprite {
-	public:
+	struct Sprite {
 		struct Vertex {
 			glm::vec2 position;
 			glm::vec2 uv;
 		};
-
-		Sprite(float centerX, float centerY, float width, float height, float dx, float dy, float dw, float dh);
-		Sprite(float centerX, float centerY, float width, float height, const TextureRegion &region, int maxFramesX = 1, int maxFramesY = 1);
-
-		void draw(void *buffer, float x, float y, int animFrameX, int animFrameY, float rotation, float scaleX, float scaleY) const;
-	private:
-		float centerX_, centerY_, width_, height_, dx_, dy_, dw_, dh_;
-	};
+		
+		float centerX, centerY, width, height;
+		TextureRegion region;
+	}
 
 }
