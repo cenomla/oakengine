6f8213a008e2bbba6badd9209a0bb3956e881236
Coleman Engbrecht <linuxFTW49@gmail.com>
2017-09-22 15:44:57 -0600

    renamed events.h

--- core/event.h
+++ core/event.h
@@ -2,11 +2,20 @@
 
 #include <cstddef>
 
+#include "type_info.h"
+
 namespace oak {
+
 	namespace config {
 		constexpr size_t MAX_EVENTS = 128;
 	}
+
 	namespace detail {
 		struct BaseEvent {};
+	}
+
+	template<class T>
+	TypeInfo makeEventInfo(const oak::string& name) {
+		return makeTypeInfo<detail::BaseEvent, T>(name);
 	}
 }
